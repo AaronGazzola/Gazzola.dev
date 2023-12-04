46c37ad54c229504f082ae8cef41f48a33a2c175
Aaron Gazzola <aaron@gazzola.dev>
2023-12-04 22:04:01 +1100

    wait for bg image to load before animation

--- app/UI/BackgroundImage.tsx
+++ app/UI/BackgroundImage.tsx
@@ -1,14 +1,23 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 const BackgroundImage = () => {
+  const [imageIsLoaded, setImageIsLoaded] = useState(false);
   return (
     <div className="fixed inset-0 -z-10">
-      <div className="absolute inset-0 bg-black z-10 bg-overlay" />
+      <div
+        className={[
+          "absolute inset-0 bg-black z-10",
+          imageIsLoaded ? "bg-overlay" : "",
+        ].join(" ")}
+      />
       <Image
         className="h-full object-cover object-center bg-image"
         src="/bg.jpg"
         alt="Background image"
         width={7319}
         height={3910}
+        onLoadingComplete={() => setImageIsLoaded(true)}
       />
     </div>
   );
