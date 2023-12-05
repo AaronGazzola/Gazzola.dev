acb2e836de7b4c5aef738a17298e3c9c5a958f11
Aaron Gazzola <aaron@gazzola.dev>
2023-12-05 22:57:12 +1100

    header styling

--- UI/Header.tsx
+++ UI/Header.tsx
@@ -8,12 +8,14 @@ const Header = () => {
     <>
       <div
         className={[
-          "flex flex-col items-center my-5 px-5 opacity-0 expanded font-thin text-center",
+          "flex flex-col items-center my-5 px-5 opacity-0 text-center uppercase",
           bgIsLoaded ? "fade-in-layout" : "",
         ].join(" ")}
       >
         <h1 className="text-xxl mb-2 p-0 expanded">Aaron Gazzola</h1>
-        <p className="expanded font-thin">Full-stack TypeScript Development</p>
+        <p className="font-thin tracking-widest">
+          Full-stack TypeScript Development
+        </p>
       </div>
     </>
   );
