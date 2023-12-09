6b970852d12a9e56a402acfface1c7df18a7fcad
Aaron Gazzola <aaron@gazzola.dev>
2023-12-09 18:48:43 +1100

    remove unused onEnter

--- UI/Chat/Editor/useEditor.ts
+++ UI/Chat/Editor/useEditor.ts
@@ -4,13 +4,11 @@ const useEditor = () => {
   const [isFocused, setIsFocused] = useState(false);
   const onFocus = () => setIsFocused(true);
   const onBlur = () => setIsFocused(false);
-  const onEnter = () => console.log("enter");
   return {
     isFocused,
     contentEditableProps: {
       onFocus,
       onBlur,
-      onEnter,
     },
   };
 };
