9cbca5a44255889c8f2fbcb855ff44e977a71435
Aaron Gazzola <aaron@gazzola.dev>
2023-12-10 11:06:11 +1100

    update body height on window resize

--- hooks/useBodyHeight.ts
+++ hooks/useBodyHeight.ts
@@ -2,14 +2,16 @@
 import { useEffect } from "react";
 import useIsMounted from "./useIsMounted";
 
-// TODO: adjust height on resize
-
 const useBodyHeight = () => {
   const isMounted = useIsMounted();
   useEffect(() => {
     if (!isMounted) return;
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    const setViewportHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+    document.addEventListener("resize", setViewportHeight);
+    return () => document.removeEventListener("resize", setViewportHeight);
   }, [isMounted]);
   return null;
 };
