19ae8c8c5f3fe60ca7416358b0db152d60aa0897
Aaron Gazzola <aaron@gazzola.dev>
2024-03-11 16:28:24 +1100

    make buttons responsive; edit bio copy

--- app/ui/Bio/Bio.tsx
+++ app/ui/Bio/Bio.tsx
@@ -7,9 +7,9 @@ import Image from "next/image";
 const Bio = () => {
   return (
     <div className="flex flex-row flex-wrap items-center justify-center">
-      <div className="flex flex-col p-10 m-2 relative text-gray-200 max-w-md">
-        <div className="border border-gray-400 border-t-transparent border-r-transparent rounded-bl-[15px] absolute left-3 bottom-3 w-16 h-16"></div>
-        <div className="border border-gray-400 border-b-transparent border-l-transparent rounded-tr-[20px] absolute right-3 top-6 w-16 h-16"></div>
+      <div className="flex flex-col p-6 sm:p-10 m-2 relative text-gray-200 max-w-md">
+        <div className="border border-gray-400 border-t-transparent border-r-transparent rounded-bl-[15px] absolute left-1 sm:left-3 bottom-1 sm:bottom-3 w-16 h-16"></div>
+        <div className="border border-gray-400 border-b-transparent border-l-transparent rounded-tr-[20px] absolute right-1 sm:right-3 top-4 sm:top-6 w-16 h-16"></div>
         <h1
           className={clsx(
             sourceCodePro.className,
@@ -20,8 +20,10 @@ const Bio = () => {
         </h1>
         <p className="text-md">
           As technology evolves, the horizon expands for those who understand
-          the full spectrum of development. My role as a Technologist is to
-          enhance the efficiency and creativity of your digital endeavors.
+          the full spectrum of development. As a Full Stack Engineer, I provide
+          comprehensive solutions that cater to both the front-end and back-end
+          needs of your project, ensuring a seamless and dynamic user
+          experience.
         </p>
         <p className="text-md">
           I am insightful and autonomous, granting me the foresight and
