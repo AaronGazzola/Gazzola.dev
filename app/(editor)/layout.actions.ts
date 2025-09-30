9301d55dfec912bd8a9e1250602796c30cede8cb
Aaron Gazzola <aaron@gazzola.dev>
2025-09-30 18:49:56 +1000

    fix(layout.actions): url construction

--- app/(editor)/layout.actions.ts
+++ app/(editor)/layout.actions.ts
@@ -5,8 +5,9 @@ import { conditionalLog } from "@/lib/log.util";
 import { MarkdownData } from "./layout.types";
 
 function getAbsoluteUrl(path: string): string {
-  const baseUrl =
-    process.env.NEXT_PUBLIC_APP_URL || process.env.VERCEL_URL
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL
+    ? process.env.NEXT_PUBLIC_APP_URL
+    : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
       : "http://localhost:3000";
   return `${baseUrl}${path}`;
